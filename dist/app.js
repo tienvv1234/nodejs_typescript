1598df7a0fa437f40a47abce733e1ca8d5d9b7a2
tienvv1234 <tienvv1234@gmail.com>
2019-05-11 00:43:53 +0700

    update

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-    console.log("Server running on " + PORT);
-});
