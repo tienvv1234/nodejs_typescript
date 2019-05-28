8589c25132703dba7824604b82b2cb9a454dd3b0
tienvv1234 <tienvv1234@gmail.com>
2019-05-29 00:38:56 +0700

    update

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -4,17 +4,18 @@ import * as express from 'express';
 import { ValidationRouterError } from '../errors/ValidationRouterError';
 
 export function validation<T>(type: any): express.RequestHandler {
-    debugger
-    let validator = new Validator();
-    return (req, res, next) => {
-        let input = deserialize(type, req.body);
-        let errors = validator.validateSync(input);
-        if (errors.length > 0) {
-            const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
-            next(new ValidationRouterError(message));
-        } else {
-            req.body = input;
-            next();
-        }
+  const validator = new Validator();
+  return (req, res, next) => {
+    const input = deserialize(type, req.body);
+    const errors = validator.validateSync(input);
+    if (errors.length > 0) {
+      const message = errors
+        .map((error: ValidationError) => Object.values(error.constraints))
+        .join(', ');
+      next(new ValidationRouterError(message));
+    } else {
+      req.body = input;
+      next();
     }
+  };
 }
